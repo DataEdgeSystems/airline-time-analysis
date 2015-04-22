316c0d260c99bc6284adb54fb4b0c7dae96d7908
Luigi Patruno <luigi.patruno.91@gmail.com>
2015-04-22 17:35:46 -0400

    downloaded airport and carrier data

--- ShellScripts/download.sh
+++ ShellScripts/download.sh
@@ -1,5 +1,10 @@
 # Download the datasets and move into the data file
 
-wget http://stat-computing.org/dataexpo/2009/2008.csv.bz2
+#wget http://stat-computing.org/dataexpo/2009/2008.csv.bz2
+#mv 2008.csv.bz2 ../data/
 
-mv 2008.csv.bz2 ../data/
+wget http://stat-computing.org/dataexpo/2009/carriers.csv
+mv carriers.csv ../data/
+
+wget http://stat-computing.org/dataexpo/2009/airports.csv
+mv airports.csv ../data/
