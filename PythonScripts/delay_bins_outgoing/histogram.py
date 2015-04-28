e5a326120fc071bf2e6ae52cf1e9c0f6a21dc70c
Luigi Patruno <luigi.patruno.91@gmail.com>
2015-04-28 17:43:14 -0400

    plot

---
+++ PythonScripts/delay_bins_outgoing/histogram.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+"""
+This script generates the histogram for the delayed outgoing JFK flights
+
+@author Luigi Patruno
+@date 28 Apr 2015
+"""
+import numpy as np
+import pandas as pd
+import matplotlib
+
+
+def main():
+    # Disable display
+    matplotlib.use('Agg')
+
+    import matplotlib.pyplot as plt
+
+    file_path = '../../data/delay_bins_outgoing/part-00000'
+
+    df = pd.read_csv(file_path, sep='\t', header=None)
+    df = df.sort(0)
+
+    bins = {}
+    x_labels = []
+    y = []
+
+    for i in range( df.shape[0] ):
+        try:
+            delay = int( df.iloc[i][0] )
+            if delay < 0:
+                delay = abs( delay )	    
+	        mod = delay % 30
+	        key = (int( (delay - mod) / 30 ) * -1) - 1 #Excuse this stupid additive factor
+            else:
+                mod = delay % 30
+                key = int( (delay - mod) / 30 )
+	        #key = '%d_%d' % (quotient*30, (quotient*30)+30)
+	    if key in bins:
+	        bins[key] += int( df.iloc[i][1] )
+	    else:
+	        bins[key] = int( df.iloc[i][1] )
+        except:
+	    pass
+
+    for k in sorted(bins.keys()):
+        label = '%d_%d' % (k*30, k * 30 + 30)
+	x_labels.append( label )
+        y.append( bins[k] )
+
+
+    x = np.arange( len(y) )
+
+
+    fig, ax = plt.subplots()
+
+    fig.set_size_inches(10,6)
+
+    plt.title('Departure Delays for Outgoing Flights (binned by half hour)')
+    
+    plt.xticks( x + .25, x_labels, rotation=40)
+    plt.xlim([min(x) - .5, max(x) + 1])
+    plt.setp(ax.get_xticklabels(), rotation='vertical', fontsize=7)
+
+    # Hide the right and top spines
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['left'].set_color('gray') 
+    ax.spines['bottom'].set_color('gray') 
+    
+    # Hide tick marks
+    ax.yaxis.set_ticks_position('none')
+    ax.xaxis.set_ticks_position('none')
+
+    plt.bar(x, y, width=.5, color='navy', edgecolor='none')
+    plt.savefig("../../plots/histogram_outgoing_delays.png")
+
+
+if __name__ == '__main__':
+    main()
+
+
